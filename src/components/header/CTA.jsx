6fa4df305cbcb612c338fdaaad7331d8106239ee
JIM440 <takemjim43@gmail.com>
2023-06-01 21:02:50 +0100

    'front-end'

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-const CTA = () => {
-return (
-<div className='cta'>
-<a href="#contact" className='btn btn-primary'>Let's Talk</a>
-</div>
-)
+import CV from'../../assets//cv.pdf'
+
+function CTA() {
+  return (
+    <div className='cta'>
+        <a href={CV} download className='btn'>Download CV</a>
+        <a href='#contact' className='btn btn-primary'>let's talk</a>
+    </div>
+  )
 }
-export default CTA; //this line indicates that this component can be imported and used everywhere in our application.
+
+export default CTA 
